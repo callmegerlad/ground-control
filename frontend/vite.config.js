522f747c3459610b5597cb4094ce316841b1d905
callmegerlad <geraldkjk@gmail.com>
2026-04-03 19:19:32 +0800

    feat(frontend): add Tailwind CSS plugin and configure server settings in Vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss()
+  ],
+  server: {
+    watch: {
+      usePolling: true,
+    },
+    host: true,
+    strictPort: true,
+    hmr: {
+      clientPort: 5173,
+    },
+  },
 })
